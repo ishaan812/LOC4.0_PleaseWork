cc370e6cbaa6ab0d1086a2d872106d73eebe1e3f
Ishaan Shah <70190533+ishaan812@users.noreply.github.com>
2022-03-12 16:01:57 +0530

    landing 2

--- social_media/src/App.js
+++ social_media/src/App.js
@@ -1,7 +1,7 @@
 import Signup from './components/signuppage/signup.jsx';
-
 import Landing from './components/landing/landing.jsx';
 import {BrowserRouter as Router,Route,Routes} from "react-router-dom"
+import './App.css'
 
 function App() {
   return (
--- social_media/src/components/landing/landing.jsx
+++ social_media/src/components/landing/landing.jsx
@@ -3,13 +3,17 @@ import './landing.scss'
 
 export default function landing() {
   return (
-    <div className='landing'>
+    <div className="landing">
         <div className="left">
             <div className="form">
-                <label>Enter username:</label>
-                <input placeholder='Username'></input>
-                <label>Enter Password:</label>
-                <input placeholder='Password'></input>
+                <div className="row">
+                    <label>Enter username:</label>
+                    <input placeholder='Username'></input>
+                </div>
+                <div className="row">
+                    <label>Enter Password:</label>
+                    <input placeholder='Password'></input>
+                </div>
             </div>
         </div>
         <div className="right">
