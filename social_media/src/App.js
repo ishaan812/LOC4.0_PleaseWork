09a3b65ec66c1c2accd581687fca8db6f8e7105a
dhruvkud13 <dhruvkudalkar13@gmail.com>
2022-03-12 15:08:12 +0530

    commit

--- social_media/src/App.js
+++ social_media/src/App.js
@@ -1,12 +1,13 @@
 import Signup from './components/signuppage/signup.jsx';
-import Landing from './components/landing/landing.jsx';
-
+// import Landing from './components/landing/landing.jsx';
+// import {BrowserRouter as Router,Route,Routes} from "react-router-dom"
 function App() {
   return (
     <div >
       <Signup />
       
     </div>
+
   );
 }
 
