3053d17d55cd1f805999f13a1381b99b471cc10d
Ishaan Shah <70190533+ishaan812@users.noreply.github.com>
2022-03-12 22:52:31 +0530

    fucking gradienty lesgo

--- social_media/src/components/landing/landing.jsx
+++ social_media/src/components/landing/landing.jsx
@@ -5,8 +5,30 @@ import {Link} from "react-router-dom";
 export default function landing() {
   return (
     <div className="landing">
-        <Link to="/signin"><button className='btn'>Login</button></Link>
-        <Link to="/signup"><button className='btn'>Signup</button></Link>
-    </div>            
+      <div className="page">
+        <div className="wrapper">
+          <div className="left">
+            
+          </div>
+          <div className="middle">
+            <div className="middlewrap">
+              <div className="name">Stalkr</div>
+              <div className="description">Social Media Analytics for the ultimate influencer</div>
+            </div>
+            <div className="buttons">
+              <Link to="/signin"><button className='btn'>Login</button></Link>
+              <Link to="/signup"><button className='btn'>Signup</button></Link>
+            </div>
+          </div>
+          <div className="right">
+            
+          </div>
+          </div>
+        </div>
+      </div>
+      // {/* <div className="buttons">
+      //   
+      // </div> */}
+               
   )
 }
