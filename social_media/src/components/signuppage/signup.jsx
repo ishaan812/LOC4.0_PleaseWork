1e7eeab883f785f64f60977eef1a5068931c5cfd
Ishaan Shah <70190533+ishaan812@users.noreply.github.com>
2022-03-12 18:22:45 +0530

    signup done
    yay

--- social_media/src/components/signuppage/signup.jsx
+++ social_media/src/components/signuppage/signup.jsx
@@ -1,8 +1,25 @@
-import React from 'react';
-import './signup.css'
+import axios from 'axios';
+import React,{ useState, useEffect } from 'react';
+import './signup.css';
 
 
 const Form = () => {
+  const [EmailID,SetEmailID]=useState('');
+  const [Name,SetName]=useState('');
+  const [Password,SetPassword]=useState('');
+  const [ConfirmPassword,SetConfirmPassword]=useState('');
+
+  const submit = (e) => {
+    e.preventDefault();
+    if(ConfirmPassword!==Password) {alert("Password is not the same as confirm password");};
+    axios.post('http://localhost:5000/users/add', 
+        {
+          "name": Name,
+          "emailid": EmailID,
+          "password": Password
+        });
+  }
+
   return (
     <div className='signup'>
         
@@ -12,26 +29,26 @@ const Form = () => {
         <br></br>
         <br></br>
         <tr><td>NAME: </td>
-            <td ><input type='text' /></td>
+            <td ><input type='text' value={Name} onChange={event=>SetName(event.target.value)} /></td>
             </tr>
             <br></br>
             <br></br>
             <tr><td>EMAIL-ID: </td>
-            <td><input type='text' /></td>
+            <td><input type='email' value={EmailID} onChange={event=>SetEmailID(event.target.value)} /></td>
             </tr>
             <br></br>
             <br></br>
             <tr><td>PASSWORD: </td>
-            <td><input type='text' /></td>
+            <td><input type='password' value={Password} onChange={event=>SetPassword(event.target.value)} /></td>
             </tr>
             <br></br>
             <br></br>
             <tr><td>CONFIRM-PASSWORD: </td>
-            <td><input type='text' /></td>
+            <td><input type='password' value={ConfirmPassword} onChange={event=>SetConfirmPassword(event.target.value)} /></td>
             </tr>
             <br></br>
             <br></br>
-            <tr><td colSpan={2}><button className='but'> SIGN UP </button></td></tr>
+            <tr><td colSpan={2}><button className='but' onClick={e=>submit(e)}> SIGN UP </button></td></tr>
         </table>
     
     </div>
