57fb14b08f19f51a987cd005704882870097fcaf
Ishaan Shah <70190533+ishaan812@users.noreply.github.com>
2022-03-13 10:31:47 +0530

    asaa
    sss

--- social_media/src/components/Instagrampage/instagram.jsx
+++ social_media/src/components/Instagrampage/instagram.jsx
@@ -8,7 +8,7 @@ const Instagram = () => {
       
       const list1 = ["Retail", "Food and Beverage", "Business, Travel and Tourism", "Healthcare", "Education", "Tech Media and Entertainment"];
      
-      const [DropDownList,SetDropDownList]=useState("");
+      const [DropDownList,SetDropDownList]=useState("Retail");
 
      
      
@@ -69,12 +69,29 @@ const Instagram = () => {
       <option value="Tech Media and Entertainment">Tech Media and Entertainment</option>
     </select>
     {
-      DropDownList === "Retail" ? <p>Hello World!</p> :
-      (DropDownList ==="Food and Beverage" ? <p>Hello World 1</p> : 
-      (DropDownList ==="Healthcare" ? <p>Hello World 1</p> :
-      (DropDownList ==="Education" ? <p>Hello World 1</p> :
-      (DropDownList ==="Travel and Tourism" ? <p>Hello World 1</p> :
-      (DropDownList ==="Tech Media and Entertainment" ? <p>Hello World 1</p>: <p>" "</p>)))))
+      DropDownList === "Retail" ? <p><b>Best time to post on Instagram:</b> Wednesday afternoon (3 pm) & Friday midday (11 am – 12 pm)<br />
+      Ultimate Best Day: Wednesday<br />
+      Worst Day: Sunday – Makes sense as most people are being present in real life on the weekends. <br />
+      Lowest Engagement: Late Night & Early Mornings – Most people sleep during these hours and are not active on social media<br />
+      </p> :
+      (DropDownList ==="Food and Beverage" ? <p><b>Best time to post on Instagram:</b> Wednesday, Thursday, Friday, Saturday- 
+      Best Hours: Noon- Most people check their phones around lunch time-
+      Most Engaging Day: Friday kicks off the weekend and plans for reservations spike
+      Worst Days for Engagement: Saturday & Sundays</p> : 
+      (DropDownList ==="Healthcare" ? <p><b>Best time to post on Instagram:</b> Fridays, Tuesdays, and Wednesdays
+      Most Engaging: Between 11am & 1pm, and between 9am and 10am.</p> :
+      (DropDownList ==="Education" ? <p><b>Best time to post on Instagram:</b> Tuesday at 8 am 
+      Most Consistent Engagement: Weekdays from 9 am to 3 pm 
+      Worst Days for Engagement: Saturday & Sundays have minimal engagement in most industries</p> :
+      (DropDownList ==="Travel and Tourism" ? <p><b>Best time to post on Instagram:</b> Friday mornings at 10 am 
+      Most Consistent Engagement: Wednesday through Saturday (10 am – 8 pm)
+      Highest Engagement: Wednesday & Friday mid morning and Saturday nights (6 pm – 8 pm)
+      Worst Days for Engagement: Sunday due to the lack of activity on Instagram on this day.</p> :
+      (DropDownList ==="Tech Media and Entertainment" ? <p><b>Best time to post on Instagram:</b> Wednesday morning at 6 am or 9 am
+      Best time to post on Instagram: Wednesday mid-morning from 10-11 am, Friday morning from 7-10 am and Saturday morning from 7-11 am. 
+      Worst Day: Sunday-Most people do not check social as often on the weekends in this industry.<b>Best time to post on Instagram:</b> Friday morning (9 am)
+Best Engagement: Wednesday, Thursday and Friday during work hours (8-5 pm)
+Lowest Engagement: Saturday and Sunday – Most people have these days off from work, are more present in real life activities and therefore not as active on social media</p>: <p>" "</p>)))))
     }
     
     
